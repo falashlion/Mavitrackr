a0e2eafe4ff575de704d363ced6955e0ecae8f27
falashlion <forchufelix@gmail.com>
2023-07-21 08:20:23 -0400

    first_push

---
+++ database/migrations/2023_07_21_100453_create_kpis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kpis', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->uuid('strategic_domains_id');
+            $table->foreign('strategic_domains_id')->references('id')->on('strategic_domains')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kpis');
+    }
+};
